ca4e4256e7a74d16469bd19debc98628673ffa16
chaoxin.lu <chaoxin258@gmail.com>
2025-09-04 14:55:23 +0800

    Refactor K8sDeployService to simplify variable replacement in deployment template, enhancing readability and maintainability of the deployment logic.

--- src/xyz/dev/ops/deploy/K8sDeployService.groovy
+++ src/xyz/dev/ops/deploy/K8sDeployService.groovy
@@ -38,11 +38,11 @@ class K8sDeployService implements Serializable {
                     script.sh "cat deployment.tpl"
                     def deployTemplate = script.readFile(encoding: "UTF-8", file: "deployment.tpl")
                     def deployment = deployTemplate
-                            .replaceAll("\$\\{APP_NAME\\}", params.serviceName)
-                            .replaceAll("\$\\{NAMESPACE\\}", params.namespace)
-                            .replaceAll("\$\\{DOCKER_REPOSITORY\\}", params.dockerRepository)
-                            .replaceAll("\$\\{IMAGE_NAME\\}", params.imageName)
-                            .replaceAll("\$\\{VERSION\\}", params.version)
+                            .replaceAll('${APP_NAME}', params.serviceName)
+                            .replaceAll('${NAMESPACE}', params.namespace)
+                            .replaceAll('${DOCKER_REPOSITORY}', params.dockerRepository)
+                            .replaceAll('${IMAGE_NAME}', params.imageName)
+                            .replaceAll('${VERSION}', params.version)
                     script.writeFile(encoding: 'UTF-8', file: './deploy.yaml', text: deployment)
                 }
 
