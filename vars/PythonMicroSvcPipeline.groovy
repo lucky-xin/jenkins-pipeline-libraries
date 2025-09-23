2b94dd0fd7b8d75fb544d338bf4a277de608f3b1
chaoxin.lu <chaoxin258@gmail.com>
2025-09-23 17:51:13 +0800

    fix(pipeline): 更新 uv.lock 中的索引 URL
    - 将 uv.lock 文件中的索引 URL 从 8081 端口更改为 9081 端口- 保证使用正确的包索引源进行依赖安装- 避免因索引 URL 错误导致构建失败的问题

--- vars/PythonMicroSvcPipeline.groovy
+++ vars/PythonMicroSvcPipeline.groovy
@@ -118,6 +118,7 @@ def call(Map<String, Object> config) {
                                     HIDDEN_IMPORTS=\$(grep -E '^[A-Za-z0-9_.-]+' requirements.txt | sed -E 's/[<>=!].*\$//' | sed -E 's#^(.+)\$#--hidden-import=\\1 #' | tr -d '\\n')
                                 elif [ -f uv.lock ]; then
                                     # 使用 uv + uv.lock 安装
+                                    sed -i 's#@http://8.145.35.103:8081#http://8.145.35.103:9081#g' uv.lock
                                     export UV_INDEX_URL="\$INDEX_URL"
                                     export UV_EXTRA_INDEX_URL="\$INDEX_URL"
                                     uv sync --no-dev --frozen
