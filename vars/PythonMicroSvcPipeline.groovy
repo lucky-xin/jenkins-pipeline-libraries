a50182361eb2504f25fa590992e717b0a096b84a
chaoxin.lu <chaoxin258@gmail.com>
2025-09-23 17:23:07 +0800

    fix(pipeline): correct syntax in hidden imports extraction
    - Updated the awk command syntax in the hidden imports extraction for both requirements.txt and uv.lock to ensure proper execution.
    - Ensured consistent formatting for better readability and maintainability.

--- vars/PythonMicroSvcPipeline.groovy
+++ vars/PythonMicroSvcPipeline.groovy
@@ -115,12 +115,12 @@ def call(Map<String, Object> config) {
                                   # 使用 pip + requirements.txt 安装
                                   pip install --timeout 60 --no-cache-dir --index-url "\$INDEX_URL" --trusted-host "\$HOST" -r requirements.txt
                                   # 自动提取 requirements.txt 模块并作为 hidden-import
-                                  HIDDEN_IMPORTS=\$(cat requirements.txt | grep -v "==" | awk '｛printf "--hidden-import=%s "， \$1｝')
+                                  HIDDEN_IMPORTS=\$(cat requirements.txt | grep -v "==" | awk '{printf "--hidden-import=%s ", \$1}')
                                 elif [ -f uv.lock ]; then
                                   # 使用 uv + uv.lock 安装
                                   export UV_INDEX_URL="\$INDEX_URL"
                                   export UV_EXTRA_INDEX_URL="\$INDEX_URL"
-                                  HIDDEN_IMPORTS=\$(uv list | grep -v "==" | awk '｛printf "--hidden-import=%s "， \$1｝')
+                                  HIDDEN_IMPORTS=\$(uv list | grep -v "==" | awk '{printf "--hidden-import=%s ", \$1}')
                                   uv sync --no-dev --frozen
                                 fi
 
